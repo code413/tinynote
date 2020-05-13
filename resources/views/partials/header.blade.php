d4ca19d22276e3236683d488642d2a8491a915bb
farzadjafari <farzad@4thirteen.com.my>
2020-05-13 11:31:45 -0400

    Added list of uploads

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,15 +5,21 @@
         </a>
     </div>
 
+
     @guest
         <a href="{{ route('login') }}">Login</a>
     @endguest
 
     @auth
-        <form action="{{ route('logout') }}" method="POST">
-            @csrf
-            <button>Logout</button>
-        </form>
+        <div class="flex">
+            <form class="mx-5" action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button>Logout</button>
+            </form>
+
+            <a href="{{ route('uploads.index') }}">Your List</a>
+        </div>
     @endauth
+
 </nav>
 
