0fefa87665a110e6701e8e6cb6ad0808b8021e9b
Matt Daneshvar <matt@4thirteen.com.my>
2020-06-23 18:58:33 +0800

    Fix #11

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,8 @@
 <header class="flex items-center px-8 py-4 ">
-    <a href="{{ urL('/') }}" class="mb-4 font-bold">Tiny Note</a>
+    <a href="{{ urL('/') }}" class="mb-4 font-bold flex items-center">
+        <img src="{{ url('img/logo.png') }}" alt="Tiny Note" class="mr-3" style="width:1.5rem; height: 1.5rem;">
+        Tiny Note
+    </a>
 
     <div class="ml-auto">
         @guest
