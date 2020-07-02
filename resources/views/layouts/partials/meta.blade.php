0ff71d14109dc0078119990ae821e4c81aa89b4b
farzadjafari <farzad@4thirteen.com.my>
2020-07-02 14:07:13 -0400

    Added og image

--- resources/views/layouts/partials/meta.blade.php
+++ resources/views/layouts/partials/meta.blade.php
@@ -3,5 +3,7 @@
 <meta name="description" content="@yield('description')">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<meta property="og:image" content="{{ asset('img/tinynote.png') }}">
+
 
 
