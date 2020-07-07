5ca1833ee638c43ebc4ea604ab1309070deace0d
farzadjafari <farzad@4thirteen.com.my>
2020-07-07 13:12:27 -0400

    Style refinement

--- resources/js/components/Links.vue
+++ resources/js/components/Links.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col flex-1 py-12 justify-center" style="background:#ece9e6;">
+    <div class="flex flex-col flex-1 py-12 justify-center md:justify-start md:py-2" style="background:#ece9e6;">
         <a class="flex items-center bg-white p-5 rounded mx-2 shadow" href="/uploads">
             <i data-feather="image" class="mr-3"></i>
             My Visuals
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -11,9 +11,10 @@
                     </div>
 
                     @if(auth()->user()->email === null)
-                    <div class="bg-purple-200 px-4 py-3 text-sm text-purple-900">
-                        Update your email so that you can save your uploads permanently and access them on all your devices.
-                    </div>
+                        <div class="bg-purple-200 px-4 py-3 text-sm text-purple-900">
+                            Update your email so that you can save your uploads permanently and access them on all your
+                            devices.
+                        </div>
                     @endif
 
                     <form class="w-full p-6" method="POST" action="{{ route('users.update', [auth()->user()]) }}">
@@ -25,7 +26,10 @@
                                 {{ __('Name') }}:
                             </label>
 
-                            <input id="name" type="text" class="form-input w-full @error('name')  border-red-500 @enderror" name="name" value="{{ auth()->user()->name ? auth()->user()->name : old('email') }}" required autocomplete="name" autofocus>
+                            <input id="name" type="text"
+                                   class="form-input w-full @error('name')  border-red-500 @enderror" name="name"
+                                   value="{{ auth()->user()->name ? auth()->user()->name : old('email') }}" required
+                                   autocomplete="name" autofocus>
 
                             @error('name')
                             <p class="text-red-500 text-xs italic mt-4">
@@ -34,20 +38,31 @@
                             @enderror
                         </div>
 
-                        @if(!isset(auth()->user()->email))
-                        <div class="flex flex-wrap mb-6">
-                            <label for="email" class="block text-gray-700 text-sm font-bold mb-2">
-                                {{ __('E-Mail Address') }}:
-                            </label>
+                        @if(isset(auth()->user()->email))
+                            <div class="flex flex-wrap mb-6">
+                                <p class="block text-gray-700 text-sm font-bold mb-2">
+                                    {{ __('E-Mail Address') }}:
+                                </p>
 
-                            <input id="email" type="email" class="form-input w-full @error('email') border-red-500 @enderror" name="email" value="{{ auth()->user()->email ?  auth()->user()->email : old('email') }}" required autocomplete="email">
+                                <p class="form-input w-full bg-gray-100">{{ auth()->user()->email ??  '' }}</p>
+                            </div>
+                        @else
+                            <div class="flex flex-wrap mb-6">
+                                <label for="email" class="block text-gray-700 text-sm font-bold mb-2">
+                                    {{ __('E-Mail Address') }}:
+                                </label>
 
-                            @error('email')
-                            <p class="text-red-500 text-xs italic mt-4">
-                                {{ $message }}
-                            </p>
-                            @enderror
-                        </div>
+                                <input id="email" type="email"
+                                       class="form-input w-full @error('email') border-red-500 @enderror" name="email"
+                                       value="{{ auth()->user()->email ?  auth()->user()->email : old('email') }}"
+                                       required autocomplete="email">
+
+                                @error('email')
+                                <p class="text-red-500 text-xs italic mt-4">
+                                    {{ $message }}
+                                </p>
+                                @enderror
+                            </div>
                         @endif
 
                         <div class="flex flex-wrap mb-6">
@@ -55,7 +70,9 @@
                                 {{ __('Password') }}:
                             </label>
 
-                            <input id="password" type="password" class="form-input w-full @error('password') border-red-500 @enderror" name="password" required autocomplete="new-password">
+                            <input id="password" type="password"
+                                   class="form-input w-full @error('password') border-red-500 @enderror" name="password"
+                                   required autocomplete="new-password">
 
                             @error('password')
                             <p class="text-red-500 text-xs italic mt-4">
@@ -65,7 +82,8 @@
                         </div>
 
                         <div class="flex flex-wrap">
-                            <button type="submit" class="inline-block align-middle text-center select-none border font-bold whitespace-no-wrap py-2 px-4 rounded text-base leading-normal no-underline text-gray-100 bg-purple-600 hover:bg-purple-700">
+                            <button type="submit"
+                                    class="inline-block align-middle text-center select-none border font-bold whitespace-no-wrap py-2 px-4 rounded text-base leading-normal no-underline text-gray-100 bg-purple-600 hover:bg-purple-700">
                                 Submit
                             </button>
                         </div>
